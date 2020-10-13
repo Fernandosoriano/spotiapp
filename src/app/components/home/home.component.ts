5bb1216f60a4bbf44e12a3b65da9206df8f3cf70
Fernandosoriano <fersoriano@ciencias.unam.mx>
2020-10-13 11:04:04 -0500

    borrado de el ejemplo de peticiones htttp, ya que no es lo indicado trabajarlo así

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -9,12 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   paises: any[] = [];
 
-  constructor(private http:HttpClient) { // 2 paso necesario para poder usar la información arrojada po rla petición http , en este componente de angular
-    console.log('constructor del home hecho ')
-  this.http.get('https://restcountries.eu/rest/v2/lang/es').subscribe((resp:any) =>{  //3 el subscribe sirve para jalar los datos, en la variable resp tienes disponible toda la información,  puede tener cualquier nombre esta variable  
-  this.paises = resp;
-    console.log(resp);
-  })
+  constructor() {
+    
    }
 
   ngOnInit(): void {
